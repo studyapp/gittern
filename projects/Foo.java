82ae1e49b88f64f8e4594e433465b683a686661d
준영 <junyoung.plum@gmail.com>
2014-02-24 17:36:20 +0900

    Add Java files

---
+++ projects/Foo.java
@@ -0,0 +1,10 @@
+public class Foo {
+    private String bar = "BAR";
+    public String getBar() {
+        return bar;
+    }
+    public static void main(String args[]) {
+        Foo foo = new Foo();
+        System.out.println(foo.getBar());
+    }
+}
