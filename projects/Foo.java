462f042e0c5910b93aed4b660aaae74e2cf04c26
준영 <junyoung.plum@gmail.com>
2014-02-24 17:51:28 +0900

    커밋 메시지 여러줄 잘 되나여?? 하핫 시프트+엔터를 하면 여러줄을 쓸 수가 있어요

--- projects/Foo.java
+++ projects/Foo.java
@@ -3,8 +3,14 @@ public class Foo {
     public String getBar() {
         return bar;
     }
+    
     public static void main(String args[]) {
         Foo foo = new Foo();
         System.out.println(foo.getBar());
+        System.out.println(foo.hashCodeTest());
+    }
+
+    public int hashCodeTest() {
+        return System.identityHashCode(null);
     }
 }
